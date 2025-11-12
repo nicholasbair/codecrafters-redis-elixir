8b341724032472cdf5d5ca585b88e100a354063f
nickbair <bairinc@gmail.com>
2025-11-12 16:09:36 -0700

    codecrafters submit [skip ci]

--- lib/main.ex
+++ lib/main.ex
@@ -6,7 +6,12 @@ defmodule Server do
   use Application
 
   def start(_type, _args) do
-    Supervisor.start_link([{Task, fn -> Server.listen() end}], strategy: :one_for_one)
+    children = [
+      {Task.Supervisor, name: Server.MessageSupervisor},
+      Supervisor.child_spec({Task, fn -> Server.listen() end}, restart: :permanent)
+    ]
+
+    Supervisor.start_link(children, strategy: :one_for_one)
   end
 
   @doc """
@@ -18,9 +23,22 @@ defmodule Server do
 
     # Since the tester restarts your program quite often, setting SO_REUSEADDR
     # ensures that we don't run into 'Address already in use' errors
-    {:ok, socket} = :gen_tcp.listen(6379, [:binary, active: false, reuseaddr: true])
+    {:ok, socket} = :gen_tcp.listen(6379, [:binary, packet: :raw, active: false, reuseaddr: true])
+    loop_acceptor(socket)
+  end
+
+  defp loop_acceptor(socket) do
     {:ok, client} = :gen_tcp.accept(socket)
+    {:ok, pid} = Task.Supervisor.start_child(Server.MessageSupervisor, fn -> handle_message(client) end)
+    :ok = :gen_tcp.controlling_process(client, pid)
+
+    loop_acceptor(socket)
+  end
+
+  defp handle_message(client) do
+    :gen_tcp.recv(client, 0)
     :gen_tcp.send(client, "+PONG\r\n")
+    handle_message(client)
   end
 end
 
